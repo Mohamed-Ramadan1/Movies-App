97f75a79326dfc4c8965b7b4ae878a00d7a0359c
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-07-22 14:27:26 +0300

    Ending featching movie data and display it .

--- src/Components/SelectedMovie.js
+++ src/Components/SelectedMovie.js
@@ -1,10 +1,75 @@
+import { useEffect, useState } from "react";
+import StarRating from "./StarRating";
+const KEY = "a48f5736";
 const SelectedMovie = ({ selectedId, onCloseMovei }) => {
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [userRating, setUserRating] = useState("");
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
+  useEffect(() => {
+    const getMovieDitails = async () => {
+      setIsLoading(true);
+      const result = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+      );
+      const data = await result.json();
+      setMovie(data);
+      setIsLoading(false);
+    };
+    getMovieDitails();
+  }, [selectedId]);
   return (
     <div className="details">
-      <button className="btn-back" onClick={onCloseMovei}>
-        &larr;
-      </button>
-      {selectedId}
+      {isLoading ? (
+        <p className="loader">Loading...</p>
+      ) : (
+        <>
+          <header>
+            <button className="btn-back" onClick={onCloseMovei}>
+              &larr;
+            </button>
+            <img src={poster} alt={`Poster of ${movie} movie`} />
+            <div className="details-overview">
+              <h2>{title}</h2>
+              <p>
+                {released} &bull; {runtime}
+              </p>
+              <p>{genre}</p>
+              <p>
+                <span>⭐️</span>
+                {imdbRating} IMDb rating
+              </p>
+            </div>
+          </header>
+          <section>
+            <div className="rating">
+              <StarRating
+                maxRating={10}
+                size={24}
+                onSetRating={setUserRating}
+              />
+            </div>
+            <p>
+              <em>{plot}</em>
+            </p>
+            <p>Starring {actors}</p>
+            <p>Directed by {director}</p>
+          </section>
+        </>
+      )}
     </div>
   );
 };
